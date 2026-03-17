76fb428d498b7d46d0c7d667701835ef8c29e311
HoangLinhDev <hoanglinh.codes@gmail.com>
2026-03-17 09:28:55 +0700

    fix(api): take userId (creat, approve ticket)

--- api/src/main/java/com/intern/hub/ticket/api/controller/TicketCommandController.java
+++ api/src/main/java/com/intern/hub/ticket/api/controller/TicketCommandController.java
@@ -9,12 +9,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.intern.hub.library.common.dto.ResponseApi;
-import com.intern.hub.starter.security.annotation.Authenticated;
 import com.intern.hub.starter.security.annotation.HasPermission;
 import com.intern.hub.starter.security.entity.Action;
 import com.intern.hub.ticket.api.dto.request.ApproveTicketRequest;
 import com.intern.hub.ticket.api.dto.request.CreateTicketRequest;
 import com.intern.hub.ticket.api.dto.response.TicketResponse;
+import com.intern.hub.ticket.api.util.UserContext;
 import com.intern.hub.ticket.core.domain.model.TicketModel;
 import com.intern.hub.ticket.core.domain.model.command.ApproveTicketCommand;
 import com.intern.hub.ticket.core.domain.model.command.CreateTicketCommand;
@@ -33,11 +33,12 @@ public class TicketCommandController {
     private final CreateTicketUsecase createTicketUsecase;
 
     @PostMapping
-    @Authenticated
+    // @Authenticated
     @Transactional
     public ResponseApi<TicketResponse> createTicket(
-            @RequestHeader("X-User-Id") Long userId,
             @Valid @RequestBody CreateTicketRequest request) {
+
+        Long userId = UserContext.requiredUserId();
 
         CreateTicketCommand command = new CreateTicketCommand(userId, request.ticketTypeId(), request.payload());
         TicketModel createdTicket = createTicketUsecase.create(command);
@@ -50,8 +51,9 @@ public class TicketCommandController {
     @Transactional
     public ResponseApi<?> approveTicket(
             @PathVariable Long ticketId,
-            @RequestHeader("X-User-Id") Long approverId,
             @Valid @RequestBody ApproveTicketRequest request) {
+
+        Long approverId = UserContext.requiredUserId();
 
         ApproveTicketCommand command = new ApproveTicketCommand(
                 ticketId,
