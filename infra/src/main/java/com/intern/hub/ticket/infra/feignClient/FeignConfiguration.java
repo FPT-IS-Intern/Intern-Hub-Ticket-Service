ef73ed2d55b58937c2553a1476eb9d5516b6779a
GWUY <buigiahuy1506@gmail.com>
2026-03-24 20:00:26 +0700

    remove EnableFeignClients in feign config

--- infra/src/main/java/com/intern/hub/ticket/infra/feignClient/FeignConfiguration.java
+++ infra/src/main/java/com/intern/hub/ticket/infra/feignClient/FeignConfiguration.java
@@ -1,14 +1,12 @@
 package com.intern.hub.ticket.infra.feignClient;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import feign.RequestInterceptor;
 
 @Configuration
-@EnableFeignClients(basePackages = "com.intern.hub.ticket.infra.feignClient")
 public class FeignConfiguration {
 
     @Value("${security.internal-secret}")
