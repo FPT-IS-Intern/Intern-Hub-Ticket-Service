ff0cda457d9c37338d284bc961e8882b1e93150c
GWUY <buigiahuy1506@gmail.com>
2026-03-24 21:22:14 +0700

    fix(feign): add multipart encoder for DMS file upload

---
+++ infra/src/main/java/com/intern/hub/ticket/infra/feignClient/FeignMultipartConfig.java
@@ -0,0 +1,15 @@
+package com.intern.hub.ticket.infra.feignClient;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import feign.codec.Encoder;
+import feign.form.spring.SpringFormEncoder;
+
+@Configuration
+public class FeignMultipartConfig {
+
+    @Bean
+    public Encoder feignFormEncoder() {
+        return new SpringFormEncoder();
+    }
+}
--- infra/src/main/java/com/intern/hub/ticket/infra/feignClient/InternalUploadDirectClient.java
+++ infra/src/main/java/com/intern/hub/ticket/infra/feignClient/InternalUploadDirectClient.java
@@ -1,7 +1,5 @@
 package com.intern.hub.ticket.infra.feignClient;
 
-import com.intern.hub.library.common.dto.ResponseApi;
-import com.intern.hub.ticket.infra.feignClient.dto.DmsDocumentClientModel;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,7 +7,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
-@FeignClient(name = "dms", url = "${services.dms.url}")
+import com.intern.hub.library.common.dto.ResponseApi;
+import com.intern.hub.ticket.infra.feignClient.dto.DmsDocumentClientModel;
+
+@FeignClient(
+  name = "dms",
+  url = "${services.dms.url}",
+  configuration = { FeignConfiguration.class, FeignMultipartConfig.class }
+)
 public interface InternalUploadDirectClient {
 
   @PostMapping(value = "/dms/internal/direct/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
