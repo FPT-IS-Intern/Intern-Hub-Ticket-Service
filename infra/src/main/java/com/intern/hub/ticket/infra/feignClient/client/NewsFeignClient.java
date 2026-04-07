29c8474f283020c04be6f198624a7f49ce4a84ea
vominhdat68 <datminhvo0901@gmail.com>
2026-04-07 21:36:52 +0700

    fix(ticket-be): route news approval callback via gateway api by default

--- infra/src/main/java/com/intern/hub/ticket/infra/feignClient/client/NewsFeignClient.java
+++ infra/src/main/java/com/intern/hub/ticket/infra/feignClient/client/NewsFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "news-service", url = "${service.news.url:http://localhost:8080}")
+@FeignClient(name = "news-service", url = "${service.news.url:${services.gateway.url:http://localhost:8765}/api}")
 public interface NewsFeignClient {
 
     @PostMapping("/news/internal/tickets/{ticketId}/approved")
